f5d1cda16e7d89a8dbc949fcf3b98bbb0d8e748f
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2022-11-02 19:59:53 -0500

    added a new endpoint named selector

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var TrousersRouter = require('./routes/Trousers');
 var GridBuildRouter = require('./routes/gridbuild');
-
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Trousers',TrousersRouter);
 app.use('/gridbuild',GridBuildRouter);
+app.use('/selector',selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
