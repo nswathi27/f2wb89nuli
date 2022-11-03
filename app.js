1bff743621c076ee4829c62cff2f0149aac00dde
Swathi_Nuli_S555089 <88883122+nswathi27@users.noreply.github.com>
2022-11-02 19:28:15 -0500

    added new route and view named gridbuild

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var TrousersRouter = require('./routes/Trousers');
+var GridBuildRouter = require('./routes/gridbuild');
+
 
 var app = express();
 
@@ -21,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/Trousers',TrousersRouter);
+app.use('/gridbuild',GridBuildRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
